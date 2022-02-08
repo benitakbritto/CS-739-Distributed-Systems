2f1d5bcf211b4419d8f33b024cbbee2d4696535e
benitakbritto <benita.k.britto@gmail.com>
2022-02-07 19:09:52 -0600

    add disk seek

--- P1/part1.c
+++ P1/part1.c
@@ -10,21 +10,27 @@ References:
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 pthread_mutex_t count_mutex;
 long long count;
 
-#define CLOCK_TYPE CLOCK_REALTIME
-#define ITERATIONS 5
+#define DEBUG                   0
+#define dbgprintf(...)          if (DEBUG) { printf(__VA_ARGS__); }
+#define CLOCK_TYPE              CLOCK_REALTIME
+#define ITERATIONS              5
+#define SEEK_LOCATION           900
+#define FILE_NAME               "README.md"
 
 void printExecutionTime(struct timespec * tpStart, struct timespec * tpEnd, char *metric);
 void incrementCount();
 void accessMainMemory();
+void performDiskSeek();
 
 int main(int argc, char **argv)
 {
-    
-
     // Mutex lock/unlock
     for (int i = 0; i < ITERATIONS; i++)
         incrementCount();
@@ -33,9 +39,33 @@ int main(int argc, char **argv)
     for (int i = 0; i < ITERATIONS; i++)
         accessMainMemory();
 
+    // Disk Seek
+    for (int i = 0; i < ITERATIONS; i++)
+        performDiskSeek();
+
     return 0;
 }
 
+
+void performDiskSeek()
+{
+    struct timespec start;
+    struct timespec stop;
+    char *fsimage = FILE_NAME;
+    int fd;
+
+    if ((fd = open(fsimage, O_RDWR, 0666)) < 0)
+    {
+        dbgprintf("Failure: open() failed with errno: %d\n", errno);
+        exit(1);
+    }
+
+    clock_gettime(CLOCK_TYPE, &start);
+    lseek(fd, SEEK_LOCATION, SEEK_SET);
+    clock_gettime(CLOCK_TYPE, &stop);
+
+    printExecutionTime(&start, &stop, "Disk Seek");
+}
 
 void accessMainMemory()
 {

88d24916048266c8bdfc15a8f0ce978d86bceeac
yatharthb <51863781+yatharthb@users.noreply.github.com>
2022-03-06 11:00:14 -0600

    Update echo.bash

--- P2/test/echo.bash
+++ P2/test/echo.bash
@@ -1,8 +1,6 @@
 
-start=`date +%T.%6N`
+date +"%T.%6N" 
+for i in {1..100}; do echo "filename${i}.bmp" ; done
+date +"%T.%6N"
 
-for i in {1..100}; do echo "filename${i}.bmp"; done
-end = `date +%T.%6N`
-
-runtime=$((end-start))
 

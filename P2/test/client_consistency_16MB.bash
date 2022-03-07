70f80c090e1927053f3f51971b796bd567c1ad4e
yatharthb <51863781+yatharthb@users.noreply.github.com>
2022-03-06 18:48:56 -0600

    Update client_consistency_16MB.bash

--- P2/test/client_consistency_16MB.bash
+++ P2/test/client_consistency_16MB.bash
@@ -1,6 +1,3 @@
-#                             Online Bash Shell.
-#                 Code, Compile, Run and Debug Bash script online.
-# Write your code in this editor and press "Run" button to execute it.
 
 
 echo "testing consistency on 16 MB"

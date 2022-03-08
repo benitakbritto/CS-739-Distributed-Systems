41083b1d34499302260277136e526c34566c6950
Benita Britto <34784181+benitakbritto@users.noreply.github.com>
2022-03-08 13:32:28 -0600

    Update scale_make.bash

--- P2/test/scale/scale_make.bash
+++ P2/test/scale/scale_make.bash
@@ -1,9 +1,7 @@
 
 echo "testing make"
-# doesn't work - can't use git clone on our fs
-git clone https://github.com/mit-pdos/xv6-public.git 
-# store a simple c file and make files in ~/files and make that
-for i in {1..100}; do date +"%T.%6N" ; make ; date +"%T.%6N"; make clean; done
+
+for i in {1..100}; do echo "start" ; date +"%T.%6N" ; cat Makefile ; cat functions.c ; cat functions.h ; cat main.c ; make ; echo "end" ; date +"%T.%6N"; make clean; done
 
 
 

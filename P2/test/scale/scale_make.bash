c13e939c28de3010acda3b8f4a6f57bdeaefe4d1
Ubuntu <benita.k.britto@gmail.com>
2022-03-08 23:02:18 +0000

    add scale results

--- P2/test/scale/scale_make.bash
+++ P2/test/scale/scale_make.bash
@@ -1,8 +1,9 @@
 
 echo "testing make"
 
-for i in {1..100}; do echo "start" ; date +"%T.%6N" ; cat Makefile ; cat functions.c ; cat functions.h ; cat main.c ; make ; echo "end" ; date +"%T.%6N"; make clean; done
-
+date +"%T.%6N"
+for i in {1..100}; do cp Makefile Makefile_copy ; cp functions.c functions_copy.c ; cp functions.h functions_copy.h ; cp main.c main_copy.c ; make ; touch functions.c ;  done
+date +"%T.%6N"
 
 
 
